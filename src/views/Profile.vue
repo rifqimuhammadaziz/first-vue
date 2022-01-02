bdb6c99b39f68ac5ad0afca6ddf9165a886d30f2
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-02 20:23:11 +0700

    create new router

---
+++ src/views/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="profile">
+    <h1>This is an profile page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
