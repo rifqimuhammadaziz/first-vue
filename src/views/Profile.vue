16f835275b13a02d40f310f9599893cb8c31136a
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-03 10:11:32 +0700

    add profile view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="profile">
-    <h1>This is an profile page</h1>
-  </div>
+<div class="profile">
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <div class="">
+                <img src="../assets/rifqi.jpg" class="user-pic">
+            </div>
+            <div class="col-lg-4 text-start ms-4">
+                <h5>{{ firstName }} {{lastName}}</h5>
+                <p>{{ bio }}</p>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default {
-  name: 'Home',
-  components: {
-    
-  }
+    name: 'Home',
+    data: function() {
+        return {
+            firstName: 'Rifqi',
+            lastName: 'Muhammad Aziz',
+            bio: 'This is some content from a media component. You can replace this with any content and adjust it as needed.'
+        }
+    },
+    components: {}
 }
 </script>
+
+<style scoped>
+.user-pic {
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+}
+</style>
